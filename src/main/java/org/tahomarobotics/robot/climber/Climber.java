90849cbfb8f33a6c701fa527c159acf9aacf1c1f
milkchunks <abowe043@go.tahomasd.us>
2025-10-29 13:24:10 -0700

    finished logging statements

--- src/main/java/org/tahomarobotics/robot/climber/Climber.java
+++ src/main/java/org/tahomarobotics/robot/climber/Climber.java
@@ -1,9 +1,12 @@
 package org.tahomarobotics.robot.climber;
 
+import org.tinylog.Logger;
+
 public class Climber implements AutoCloseable {
     private final ClimberSubsystem climber;
     public Climber() {
         this(new ClimberSubsystem());
+        Logger.info("Creating new instance of Climber...");
     }
 
     Climber(ClimberSubsystem climber) {
